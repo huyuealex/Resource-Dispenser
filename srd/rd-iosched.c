a182db8702c1fa22b68372acb2ac22ce5008fc98
reallynotme <reallynotme@126.com>
2012-06-23 20:26:30 +0800

    算是部分解决了io停止的问题,但是目前需要通过"echo 'w'>/proc/rdnice" 来手动重启. 另外用来重启的代码不一定安全.

--- srd/rd-iosched.c
+++ srd/rd-iosched.c
@@ -14,16 +14,18 @@ static struct proc_dir_entry *proc_entry;
 static pid_t vippid=0;//虽然貌似有特殊进程pid为0,但是本程序vippid==0时认为其无效
 static unsigned long nextprintjiffies=0,dontuntil=0;//只有当jiffies>dontuntil后才能dispatch普通进程
 static unsigned int vipdispatch=0,normaldispatch=0;
-static unsigned int refucetime=0;
+static unsigned int refucetime=0,mergereqtime=0;
 
 struct rd_data {
+	struct request_queue *q;
 	struct list_head queue,vipqueue;
-};
+}*ndglobal;
 
 static void rd_merged_requests(struct request_queue *q, struct request *rq,
 				 struct request *next)
 {
 	list_del_init(&next->queuelist);
+	++mergereqtime;
 }
 
 static int rd_dispatch(struct request_queue *q, int force)
@@ -55,8 +57,10 @@ static int rd_dispatch(struct request_queue *q, int force)
 		return 1;
 	}
 	++refucetime;
-	printk(KERN_INFO "vd:%u,nd:%u,du=%lu,j=%lu,rt=%u",
-			vipdispatch,normaldispatch,dontuntil,jiffies,refucetime);
+	printk(KERN_INFO "vd:%u,nd:%u,du=%lu,j=%lu,rt=%u,mt=%u,fc=%i,qe=%i,vqe=%i,rqe=%i",
+			vipdispatch,normaldispatch,dontuntil,jiffies,refucetime,mergereqtime,force,
+			list_empty(&nd->queue),list_empty(&nd->vipqueue),
+			list_empty(&nd->queue)&&list_empty(&nd->vipqueue));
 	return 0;
 }
 
@@ -65,16 +69,21 @@ static void rd_add_request(struct request_queue *q, struct request *rq)
 	struct rd_data *nd = q->elevator->elevator_data;
 
 	if(vippid&&current->pid==vippid)
-		list_add_tail(&rq->queuelist, &nd->vipqueue);
-	else
 		list_add_tail(&rq->queuelist, &nd->queue);
+	else
+		list_add_tail(&rq->queuelist, &nd->vipqueue);
 }
 
 static int rd_queue_empty(struct request_queue *q)
 {
 	struct rd_data *nd = q->elevator->elevator_data;
-
-	return list_empty(&nd->queue)&&list_empty(&nd->vipqueue);
+	int retval=list_empty(&nd->vipqueue)&&(!time_after(jiffies,dontuntil)||list_empty(&nd->queue));
+	printk(KERN_INFO "du=%lu,j=%lu,qe=%i,vqe=%i,empty=%i",
+			dontuntil,jiffies,
+			list_empty(&nd->queue),list_empty(&nd->vipqueue),
+			retval);
+	//return list_empty(&nd->queue)&&list_empty(&nd->vipqueue);
+	return retval;
 }
 
 static struct request *
@@ -104,8 +113,10 @@ static void *rd_init_queue(struct request_queue *q)
 	nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
 	if (!nd)
 		return NULL;
+	nd->q=q;
 	INIT_LIST_HEAD(&nd->queue);
 	INIT_LIST_HEAD(&nd->vipqueue);
+	ndglobal=nd;
 	return nd;
 }
 
@@ -122,9 +133,9 @@ static struct elevator_type elevator_rd = {
 		.elevator_merge_req_fn		= rd_merged_requests,//待改
 		.elevator_dispatch_fn		= rd_dispatch,//
 		.elevator_add_req_fn		= rd_add_request,//
-		.elevator_queue_empty_fn	= rd_queue_empty,//
-		.elevator_former_req_fn		= rd_former_request,//
-		.elevator_latter_req_fn		= rd_latter_request,//
+		//.elevator_queue_empty_fn	= rd_queue_empty,//
+		//.elevator_former_req_fn		= rd_former_request,//
+		//.elevator_latter_req_fn		= rd_latter_request,//
 		.elevator_init_fn		= rd_init_queue,//
 		.elevator_exit_fn		= rd_exit_queue,//
 	},
@@ -146,11 +157,46 @@ static int rdatoi(const char *s)
 static int rdnice_write( struct file *filp, const char __user *buff,
 	       	unsigned long len, void *data )
 {
+	struct request_queue *q=ndglobal->q;
 	if(len>sizeof(vippidbuf)-1)
 		return -EFBIG;
 	if (copy_from_user( vippidbuf, buff, len ))
 		return -EFAULT;
 	vippidbuf[len-1]=0;//无论用户传入串是否以0结尾,保证vippidbuf以0结尾
+	if(vippidbuf[0]=='w')
+	{
+		printk(KERN_INFO "io waking up:j=%lu\n",jiffies);
+		//blk_run_queue(ndglobal->q);
+		//generic_unplug_device(ndglobal->q);
+		//下面将generic_unplug_device(ndglobal->q);展开以确定问题出在哪!
+
+		if (blk_queue_plugged(q)) {
+			int fail=0;
+			spin_lock_irq(q->queue_lock);
+			if (unlikely(blk_queue_stopped(q)))
+			{
+				printk(KERN_INFO "is blk_queue_stopped\n");
+				fail=1;
+			}
+			if (!blk_remove_plug(q) && !blk_queue_nonrot(q))
+			{
+				printk(KERN_INFO "is (!blk_remove_plug(q) && !blk_queue_nonrot(q))\n");
+				fail=1;
+			}
+			if(!fail)
+				q->request_fn(q);
+			spin_unlock_irq(q->queue_lock);
+		}
+		else
+		{
+			printk(KERN_INFO "blk_queue_plugged(q) fail\n");
+			spin_lock_irq(q->queue_lock);
+			q->request_fn(q);
+			spin_unlock_irq(q->queue_lock);
+		}
+		printk(KERN_INFO "io waked up:j=%lu\n",jiffies);
+		return len;
+	}
 	vippid=rdatoi(vippidbuf);
 	printk(KERN_INFO "new vippid=%i,jiffies=%lu\n",vippid,jiffies);
 	return len;
