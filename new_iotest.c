a6eb97a2bd3947adb19a954f9277465d36bc0d2e
Yue Hu <huyuealex@sina.com>
2012-06-26 15:27:13 +0800

    对new_iotest.c进行了改进，可将打印信息输出到log文件中

--- new_iotest.c
+++ new_iotest.c
@@ -7,12 +7,15 @@
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #define BUF_SIZE 2048
-
 //assume
 #define PAGE_SIZE 4096
 #define INTERVAL 10
+#define MSGS 10000
 
 char file_priority[6][2][50] = 
 {
@@ -26,10 +29,44 @@ char file_priority[6][2][50] =
 
 char shell_cmd[500];
 char file[100];
-//char log[10000][20]; // log功能还有待实现
+
+int segment_id;
+typedef struct{  
+	char msg[MSGS][100];  
+	int cnt;  
+}shared_msgs;
+
+char output[MSGS * 100];
+char cmd[MSGS * 100];
 
 int main(int argc, char *argv[])
 {
+	/*
+	 * 分配日志需要的进程共享内存
+	 */
+	const int SIZE = sizeof(shared_msgs);  
+	segment_id = shmget(IPC_PRIVATE, SIZE, S_IRUSR | S_IWUSR);
+	shared_msgs *msgs= (shared_msgs*) shmat(segment_id, NULL, 0);
+	msgs->cnt = 0;
+	/*
+	 * 建立互斥量
+	 */
+	int semid;
+	static struct sembuf lock={0,-1,SEM_UNDO};
+	static struct sembuf unlock={0,1,SEM_UNDO|IPC_NOWAIT};
+	if((semid = semget(998, 1, IPC_CREAT | IPC_EXCL | 0666)) == -1)
+	{
+		printf("Error: semget!\n");
+		return 1;
+	}
+	if(semctl(semid, 0, SETVAL, 1) == -1)
+	{
+		printf("Error: semctl!\n");
+		return 1;
+	}
+	/*
+	 * 生成多进程
+	 */
 	pid_t fpid;
 	int i, j, prio;
 	time_t deadline = (time_t)((int)time(NULL) + 2);
@@ -46,6 +83,24 @@ int main(int argc, char *argv[])
 		for (i = 0; i < 12; i++)
 			wait(NULL);
 		system("echo 'clear' > /proc/rdnice");
+
+		if(semctl(semid, 0, IPC_RMID, 0) == -1) // 删除互斥量
+		{
+			printf("error: semctl!\n");
+			return 1;
+		}
+		/*
+		 * 将日志输出至文件
+		 */
+		int pos = 0, len = 0;
+		for (i = 0; i < msgs->cnt; i++)
+		{
+			len = sprintf(output + pos, "%s", msgs->msg[i]);
+			pos += len;
+		}
+		sprintf(cmd, "echo '%s' > log", output);
+		system(cmd);
+		printf("Done.\n");
 		return 0;
 	}
 	fpid = getpid();
@@ -57,6 +112,12 @@ int main(int argc, char *argv[])
 	//printf("%s\n", shell_cmd);	
 	system(shell_cmd);
 	while (time(NULL) < deadline); // 等等慢的进程，大家一起来读文件
+
+	if((semid = semget(998, 1, 0)) == -1) // 读取互斥量
+	{
+		printf("Error: semget!\n");
+		return 1;
+	}
 
 	int fd, intervalPercent = INTERVAL;
 	struct timeval sTime, eTime;
@@ -85,7 +146,18 @@ int main(int argc, char *argv[])
 	{
 		if(totalRead >= nextCheckPoint)
 		{
-			printf("(pid = %d, prio = %d): %.1f%%\n", fpid, prio, (double)(totalRead) / fdstat.st_size*100);
+			if(semop(semid, &lock, 1) == -1)
+			{
+				printf("Error: semop(lock)!\n");
+				return 1;
+			}
+			sprintf(msgs->msg[msgs->cnt], "(pid = %d, prio = %d): %.1f%%\n", fpid, prio, (double)(totalRead) / fdstat.st_size*100);
+			msgs->cnt++;
+			if(semop(semid, &unlock, 1) == -1)
+			{
+				printf("Error: semop(unlock)!\n");
+				return 1;
+			}
 			nextCheckPoint += intervalByte;
 		}
 		if((currentRead = read(fd, buf, sizeof(buf)))>0)
@@ -93,10 +165,22 @@ int main(int argc, char *argv[])
 		buf[2] = buf[1026] = 2;
 	}
 	gettimeofday(&eTime, NULL);
-	printf("Process %d finished\nRead Byte:%zi\tRead times:%lims\n",
+	if(semop(semid, &lock, 1) == -1)
+	{
+		printf("Error: semop(lock)!\n");
+		return 1;
+	}
+	sprintf(msgs->msg[msgs->cnt], "Process %d finished. (File: %s, Read: %ziB, Time: %lims)\n",
 			fpid,
+			file,
 			totalRead,
 			(eTime.tv_sec-sTime.tv_sec)*1000+(eTime.tv_usec-sTime.tv_usec)/1000);
+	msgs->cnt++;
+	if(semop(semid, &unlock, 1) == -1)
+	{
+		printf("Error: semop(unlock)!\n");
+		return 1;
+	}
 	close(fd);
 	return 0;
 }
