acae07a31c4521402a3d28fb1064adc257f61fbf
Yue Hu <huyuealex@sina.com>
2012-06-27 01:00:56 +0800

    nsrd改进完成，可不再饿死低优先级进程的请求，加入阀值threshold，具体见readme

--- nsrd/rd-iosched.c
+++ nsrd/rd-iosched.c
@@ -7,12 +7,15 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
+#include <linux/kernel.h>
+#include <linux/random.h>
 //#include <linux/jiffies.h>
 #define MOD 			100003
 #define LEN 			10
 #define MAXN_PRIO 		5
 #define TIME_AFTER_DISPATCH	20
 #define TIME_RESET		30
+#define DEN			10007
 
 static char proc_writebuf[100];
 static char proc_readbuf[1000];
@@ -21,6 +24,10 @@ static unsigned long dontuntil = 0; // 只有当jiffies>dontuntil后才能dispat
 
 static unsigned cnt_processes[MAXN_PRIO + 1];
 static int last_prio = 0;
+
+static int threshold = 0; // 用来判断使用随机法还是优先级法的阀值
+static int random = 0; // 1为使用随机法，0为使用优先级法
+static int rand_yes = 0, rand_no = 0; // 测试用
 
 struct io_process {
 	pid_t pid; // 进程id
@@ -46,46 +53,87 @@ static void rd_merged_requests(struct request_queue *q, struct request *rq,
 static int rd_dispatch(struct request_queue *q, int force)
 {
 	struct rd_data *nd = q->elevator->elevator_data;
-	int i = 0;
-	for (i = MAXN_PRIO; i >= 0; i--) {
-		if (!list_empty(&nd->prio_queue[i])) {
-			if (last_prio <= i || force || time_after(jiffies, dontuntil)) {
+	int i = 0, result = 0;
+	unsigned int randNum;
+	int goodluck;
+	int s, e;
+	if (random) {
+		get_random_bytes(&randNum, sizeof(unsigned int));
+		if (randNum < 0) randNum *= -1;
+		s = randNum % (MAXN_PRIO + 1);
+		e = (s + 1) % (MAXN_PRIO + 1);
+		for (i = s; ; i = (i - 1 + (MAXN_PRIO + 1)) % (MAXN_PRIO + 1)) {
+			if (!list_empty(&nd->prio_queue[i])) {
 				struct request *rq;
 				rq = list_entry(nd->prio_queue[i].next, struct request, queuelist);
 				list_del_init(&rq->queuelist);
 				elv_dispatch_sort(q, rq);
 				cnt_processes[i]++;
-				dontuntil = jiffies + msecs_to_jiffies(TIME_AFTER_DISPATCH); // 20ms后才能dispatch普通进程的请求
-				mod_timer(&nd->resume_timer, jiffies + msecs_to_jiffies(TIME_RESET)); // 定时器在30ms后尝试重启io
+				dontuntil = jiffies + msecs_to_jiffies(TIME_AFTER_DISPATCH);
+				mod_timer(&nd->resume_timer, jiffies + msecs_to_jiffies(TIME_RESET));
 				last_prio = i;
-				return 1;
+				result += 1;
+				break;
+			}
+			if (i == e) break;
+		}
+		//if (result == 0) random = 0;
+	}
+	if (!random) {
+		for (i = MAXN_PRIO; i >= 0; i--) {
+			if (!list_empty(&nd->prio_queue[i])) {
+				if (last_prio <= i || force || time_after(jiffies, dontuntil)) {
+					struct request *rq;
+					rq = list_entry(nd->prio_queue[i].next, struct request, queuelist);
+					list_del_init(&rq->queuelist);
+					elv_dispatch_sort(q, rq);
+					cnt_processes[i]++;
+					dontuntil = jiffies + msecs_to_jiffies(TIME_AFTER_DISPATCH);
+					mod_timer(&nd->resume_timer, jiffies + msecs_to_jiffies(TIME_RESET));
+					last_prio = i;
+					result += 1;
+					break;
+				}
 			}
 		}
 	}
-	return 0;
+	get_random_bytes(&randNum, sizeof(unsigned int));
+	if (randNum < 0) randNum *= -1;
+	goodluck = randNum % DEN;
+	if (goodluck == 0) goodluck++;
+	if (goodluck <= threshold) random = 1;
+	else random = 0;
+	if (random) rand_yes++;
+	else rand_no++;
+	return result;
 }
 
 static void rd_add_request(struct request_queue *q, struct request *rq)
 {
 	struct rd_data *nd = q->elevator->elevator_data;
 	int key = current->pid % MOD;
-	int i = 0, find = 0;
+	int i = 0, prio = 0;
+
 	for (i = 0; i < LEN; i++) {
 		if (hash_process[key][i].alive == 1 && current->pid == hash_process[key][i].pid) {
-			list_add_tail(&rq->queuelist, &nd->prio_queue[hash_process[key][i].prio]);
-			find = 1;
+			prio = hash_process[key][i].prio;
 			break;
 		}
 	}
-	if (!find) {
-		list_add_tail(&rq->queuelist, &nd->prio_queue[0]);
-	}
+	list_add_tail(&rq->queuelist, &nd->prio_queue[prio]);
 }
 
 static int rd_queue_empty(struct request_queue *q)
 {
 	struct rd_data *nd = q->elevator->elevator_data;
 	int i = 0;
+	if (random) {
+		for (i = MAXN_PRIO; i >= 0; i--) {
+			if (!list_empty(&nd->prio_queue[i]))
+				return 0;
+		}
+		return 1;
+	}
 	for (i = MAXN_PRIO; i >= last_prio; i--) {
 		if (!list_empty(&nd->prio_queue[i]))
 			return 0;
@@ -122,16 +170,16 @@ static struct request *rd_latter_request(struct request_queue *q, struct request
 
 static void resume_timeout(unsigned long data)
 {
-	int i = 0;
-	printk(KERN_INFO "0-timeout, j = %lu",jiffies);
-	for (i = 0; i <= MAXN_PRIO; i++)
-		printk(KERN_INFO ", cnt_prio_%d = %u", i, cnt_processes[i]);
-	printk(KERN_INFO "\n");
+	//int i = 0;
+	//printk(KERN_INFO "0-timeout, j = %lu",jiffies);
+	//for (i = 0; i <= MAXN_PRIO; i++)
+	//	printk(KERN_INFO ", cnt_prio_%d = %u", i, cnt_processes[i]);
+	//printk(KERN_INFO "\n");
 	blk_run_queue((struct request_queue *)data);
-	printk(KERN_INFO "1-timeout, j = %lu",jiffies);
-	for (i = 0; i <= MAXN_PRIO; i++)
-		printk(KERN_INFO ", cnt_prio_%d = %u", i, cnt_processes[i]);
-	printk(KERN_INFO "\n");
+	//printk(KERN_INFO "1-timeout, j = %lu",jiffies);
+	//for (i = 0; i <= MAXN_PRIO; i++)
+	//	printk(KERN_INFO ", cnt_prio_%d = %u", i, cnt_processes[i]);
+	//printk(KERN_INFO "\n");
 }
 
 static void clear_hash(void)
@@ -141,6 +189,9 @@ static void clear_hash(void)
 		for (j = 0; j < LEN; j++)
 			hash_process[i][j].alive = 0;
 	printk(KERN_INFO "Done: clear all data from hash.\n");
+	printk(KERN_INFO "Rand_yes = %d, Rand_no = %d.\n", rand_yes, rand_no);
+	rand_yes = 0;
+	rand_no = 0;
 }
 
 static void *rd_init_queue(struct request_queue *q)
@@ -280,6 +331,25 @@ static void remove_process(const char *s)
 	return;
 }
 
+static void set_threshold(const char *s)
+{
+	int num = 0, flag = 0;
+	while((*s) != 0) {
+		if ((*s) >= '0' && (*s) <= '9') {
+			num = num * 10 + ((*s) - '0');
+			flag = 1;
+		}
+		else if (flag == 1)
+		{
+			flag = 0;
+			break;
+		}
+		s++;
+	}
+	threshold = num;
+	if (threshold > DEN) threshold = DEN;
+}
+
 static int rdnice_write( struct file *filp, const char __user *buff,
 		unsigned long len, void *data )
 {
@@ -287,7 +357,7 @@ static int rdnice_write( struct file *filp, const char __user *buff,
 		return -EFBIG;
 	if (copy_from_user(proc_writebuf, buff, len))
 		return -EFAULT;
-	proc_writebuf[len] = 0; // 无论用户传入串是否以0结尾,保证proc_writebuf以0结尾
+	proc_writebuf[len] = 0;
 	if(proc_writebuf[0] == 'w')
 	{
 		printk(KERN_INFO "io waking up:j=%lu\n",jiffies);
@@ -297,15 +367,14 @@ static int rdnice_write( struct file *filp, const char __user *buff,
 	if (proc_writebuf[0] == 'a') add_process(proc_writebuf); // 格式：add pid prio，如不加参数prio则默认设置为最高优先级MAXN_PRIO，例：add 1234 3
 	else if (proc_writebuf[0] == 'r') remove_process(proc_writebuf); // 格式：remove pid，例：remove 1234
 	else if (proc_writebuf[0] == 'c') clear_hash(); // 格式：clear
+	else if (proc_writebuf[0] == 't') set_threshold(proc_writebuf); // 格式：threshold num，num为0-10007(DEN)之间的整数，例：threshold 1000
 	return len;
 }
 
 static int rdnice_read( char *page, char **start, off_t off,int count, int *eof, void *data )
 {
-	// 测试用
 	int len, i, j, pos, length;
 	char oneline[100];
-	// 下面这个if不很清楚是干什么用的,貌似off>0表示目标缓冲区占据多页.那么就不处理直接退出
 	if (off > 0) {
 		*eof = 1;
 		return 0;
@@ -319,9 +388,12 @@ static int rdnice_read( char *page, char **start, off_t off,int count, int *eof,
 				sprintf(proc_readbuf + pos, "%s", oneline);
 				pos += length;
 			}
+	if (pos == 0) {
+		length = sprintf(proc_readbuf + pos, "No data in hash\n");
+		pos += length;
+	}
+	sprintf(proc_readbuf + pos, "Threshold: %d, Max: %d\n", threshold, DEN);
 	len = sprintf(page, "%s", proc_readbuf);
-	if (len == 0)
-		len = sprintf(page, "No data in hash\n");
 	return len;
 }
 
